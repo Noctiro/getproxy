f2d8ce58176cf087405f61e963f1e3c03d3211e5
ObcbO <49872497+ObcbO@users.noreply.github.com>
2023-09-09 00:06:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,17 @@ if [ -f "$1" ]; then
 rm -f $1
 fi
 }
-dl () {
+proxy () {
 del temp_$1.txt
-curl -o temp_$1.txt $2
-sed -i 's/^/'$1':\/\/&/g' temp_$1.txt
-echo >> temp_$1.txt
+
+curl -o temp_$1_original.txt $2
+grep -Eo "(((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))" temp_$1_original.txt > temp_$1.txt
+sed -i 's/^/'$1'://g' temp_$1.txt
+del temp_$1_original.txt
+
 cat temp_$1.txt >> $1.txt
-cat temp_$1.txt >> temp_all.txt
+cat temp_$1.txt >> all.txt
+
 del temp_$1.txt
 }
 
@@ -21,29 +25,26 @@ del socks4.txt
 del socks5.txt
 
 # http
-dl "http" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-http.txt"
-dl "http" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/http.txt"
-dl "http" "https://www.proxyscan.io/api/proxy?format=txt&type=http"
+proxy "http" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-http.txt"
+proxy "http" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/http.txt"
+proxy "http" "https://www.proxyscan.io/api/proxy?format=txt&type=http"
 # https
-dl "https" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-https.txt"
-dl "https" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/https.txt"
-dl "https" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/HTTPS_RAW.txt"
-dl "https" "https://www.proxyscan.io/api/proxy?format=txt&type=https"
+proxy "https" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-https.txt"
+proxy "https" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/https.txt"
+proxy "https" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/HTTPS_RAW.txt"
+proxy "https" "https://www.proxyscan.io/api/proxy?format=txt&type=https"
 #socks4
-dl "socks4" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-socks4.txt"
-dl "socks4" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/socks4.txt"
-dl "socks4" "https://fastly.jsdelivr.net/gh/TheSpeedX/PROXY-List@master/socks4.txt"
-dl "socks4" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/SOCKS4_RAW.txt"
-dl "socks4" "https://www.proxyscan.io/api/proxy?format=txt&type=socks4"
+proxy "socks4" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-socks4.txt"
+proxy "socks4" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/socks4.txt"
+proxy "socks4" "https://fastly.jsdelivr.net/gh/TheSpeedX/PROXY-List@master/socks4.txt"
+proxy "socks4" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/SOCKS4_RAW.txt"
+proxy "socks4" "https://www.proxyscan.io/api/proxy?format=txt&type=socks4"
 #socks5
-dl "socks5" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-socks5.txt"
-dl "socks5" "https://fastly.jsdelivr.net/gh/hookzof/socks5_list@master/proxy.txt"
-dl "socks5" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/socks5.txt"
-dl "socks5" "https://fastly.jsdelivr.net/gh/TheSpeedX/PROXY-List@master/socks5.txt"
-dl "socks5" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/SOCKS5_RAW.txt"
-dl "socks5" "https://www.proxyscan.io/api/proxy?format=txt&type=socks5"
-
-cat temp_all.txt | sort -u > all.txt
-del temp_all.txt
+proxy "socks5" "https://fastly.jsdelivr.net/gh/jetkai/proxy-list@main/online-proxies/txt/proxies-socks5.txt"
+proxy "socks5" "https://fastly.jsdelivr.net/gh/hookzof/socks5_list@master/proxy.txt"
+proxy "socks5" "https://fastly.jsdelivr.net/gh/UserR3X/proxy-list@main/online/socks5.txt"
+proxy "socks5" "https://fastly.jsdelivr.net/gh/TheSpeedX/PROXY-List@master/socks5.txt"
+proxy "socks5" "https://fastly.jsdelivr.net/gh/roosterkid/openproxylist@main/SOCKS5_RAW.txt"
+proxy "socks5" "https://www.proxyscan.io/api/proxy?format=txt&type=socks5"
 
 echo -e "\e[1;32mDone!\e[0m"
